79195a7629315165187cb9ffafc35840759df9c4
Arindam Deb <Arindamdeb2000@icloud.com>
2024-01-29 14:46:14 +0530

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,7 +1,12 @@
 // Options the user could type in
 const prompts = [
   ["hi", "hey", "hello", "good morning", "good afternoon"],
-  ["What are the primary responsibilities of the GTE team?", "In which areas does the GTE team specialize?", "How does the GTE team contribute to its respective field?", "What does GTE team do?"],
+  [
+    "What are the primary responsibilities of the GTE team?",
+    "In which areas does the GTE team specialize?",
+    "How does the GTE team contribute to its respective field?",
+    "What does GTE team do?", "GTE","Global Talent Evangelist"
+  ],
   ["what are you doing", "what is going on", "what is up"],
   ["how old are you"],
   ["who are you", "are you human", "are you bot", "are you human or bot"],
@@ -9,31 +14,36 @@ const prompts = [
   [
     "your name please",
     "your name",
-    "may i know your name",
+    "may I know your name",
     "what is your name",
-    "what call yourself"
+    "what do you call yourself"
   ],
-  ["i love you"],
+  ["I love you"],
   ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
   ["bad", "bored", "tired"],
-  ["help me", "tell me story", "tell me joke"],
+  ["help me", "tell me a story", "tell me a joke"],
   ["ah", "yes", "ok", "okay", "nice"],
-  ["bye", "good bye", "goodbye", "see you later"],
-  ["what should i eat today"],
+  ["bye", "goodbye", "see you later"],
+  ["what should I eat today"],
   ["bro"],
   ["what", "why", "how", "where", "when"],
-  ["no","not sure","maybe","no thanks"],
+  ["no", "not sure", "maybe", "no thanks"],
   [""],
-  ["haha","ha","lol","hehe","funny","joke"]
-]
+  ["haha", "ha", "lol", "hehe", "funny", "joke"]
+];
 
 // Possible responses, in corresponding order
 
 const replies = [
-  ["Hello!", "Hi!", "Hey!", "Hi there!","Howdy"],
+  ["Hello!", "Hi!", "Hey!", "Hi there!", "Howdy"],
   [
-    "The Global Talent Evangelist (CoE) team = Research Oriented Talent Identification from across the globe, purposeful nurturing of top talent to meet Adobe's business objectives."
-  ]
+    "The Global Talent Evangelist (CoE) team = Research Oriented Talent Identification from across the globe, purposeful nurturing of top talent to meet Adobe's business objectives.",
+    [
+      "a. Spearheading technological innovations",
+      "b. Advancing research and development",
+      "c. Improving user experiences"
+    ]
+  ],
   [
     "Nothing much",
     "About to go to sleep",
@@ -53,10 +63,10 @@ const replies = [
   ["Sushi", "Pizza"],
   ["Bro!"],
   ["Great question"],
-  ["That's ok","I understand","What do you want to talk about?"],
+  ["That's ok", "I understand", "What do you want to talk about?"],
   ["Please say something :("],
-  ["Haha!","Good one!"]
-]
+  ["Haha!", "Good one!"]
+];
 
 // Random for any other user input
 
@@ -67,8 +77,13 @@ const alternative = [
   "Try again",
   "I'm listening...",
   "I don't understand :/"
-]
+];
 
 // Whatever else you want :)
 
-const coronavirus = ["Please stay home", "Wear a mask", "Fortunately, I don't have COVID", "These are uncertain times"]
+const coronavirus = [
+  "Please stay home",
+  "Wear a mask",
+  "Fortunately, I don't have COVID",
+  "These are uncertain times"
+];
