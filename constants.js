7c9a32ffe4d426597f62dc7a2d78a76880cf44f6
Arindam Deb <Arindamdeb2000@icloud.com>
2024-01-31 19:19:38 +0530

    Update constants.js

--- constants.js
+++ constants.js
@@ -2,17 +2,11 @@
 const prompts = [
   ["hi", "hey", "hello", "good morning", "good afternoon"],
   ["how are you", "how is life", "how are things"],
-  ["Who supports Data, Dashboard & Reporting","Data, Dashboard & Reporting"],
+  ["Who supports Data, Dashboard & Reporting","Data,Dashboard & Reporting"],
   ["how old are you"],
   ["who are you", "are you human", "are you bot", "are you human or bot"],
   ["who created you", "who made you"],
-  [
-    "your name please",
-    "your name",
-    "may i know your name",
-    "what is your name",
-    "what call yourself"
-  ],
+  ["your name please","your name","may i know your name","what is your name","what call yourself"],
   ["i love you"],
   ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
   ["bad", "bored", "tired"],
