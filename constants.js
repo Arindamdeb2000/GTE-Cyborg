83acd0be8d147110271f6e0a52c1d70c37168ec3
Arindam Deb <Arindamdeb2000@icloud.com>
2024-01-31 23:19:35 +0530

    Update constants.js

--- constants.js
+++ constants.js
@@ -29,7 +29,7 @@ const prompts = [
   ["no","not sure","maybe","no thanks"],
   [""],
   ["haha","ha","lol","hehe","funny","joke"]
-];
+]
 
 // Possible responses, in corresponding order
 const replies = [
@@ -41,9 +41,9 @@ const replies = [
   ],
   [
     "The GTE team, led by individuals like Arindam Deb (ARI), Pooja Saxena, Mahek Mann, Lesten D’Souza, and Anushree Kulkarni, is primarily responsible for Global Executive Hiring. Their key areas of expertise and focus include executing GTE KPIs, sourcing leadership, program management, global intelligence, innovation initiatives, talent communities, executive market intelligence, as well as design and execution of strategic initiatives. Additionally, they play vital roles in managing data, dashboards, and reporting for effective decision-making within the organization.",
+    "Global Executive Hiring,Exec Market Intelligence,Innovation Initiatives",  // Moved this response to the correct position
     "Can you guess?",
-    "I don't know actually",
-    "Global Executive Hiring,Exec Market Intelligence,Innovation Initiatives"  // Moved this response to the correct position
+    "I don't know actually"
   ],
   ["72 Hours... precisely"],
   ["Global Executive Hiring,Exec Market Intelligence,Innovation Initiatives"],
